c6690b1010ec7e71caa5303cc57c8cdc28c493d1
Trudy <gcyn51a@gmail.com>
2023-03-16 20:10:04 +0300

    Minor edit

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.9",
+  // Adding this entry allows us to run the deploy script without specifying the network.
   defaultNetwork: "sepolia",
   networks: {
     sepolia: {
